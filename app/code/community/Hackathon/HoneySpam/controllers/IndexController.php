c6bfd3a8f04640646873d959515fae3e69f7b81a
Sascha Wohlgemuth <sascha.wohlgemuth@gmail.com>
2012-10-27 23:25:48 +0200

    Redirect to honeyspam error page when spam is detected

---
+++ app/code/community/Hackathon/HoneySpam/controllers/IndexController.php
@@ -0,0 +1,36 @@
+<?php
+/**
+ * Magento
+ *
+ * NOTICE OF LICENSE
+ *
+ * This source file is subject to the Open Software License (OSL 3.0)
+ * that is bundled with this package in the file LICENSE.txt.
+ * It is also available through the world-wide-web at this URL:
+ * http://opensource.org/licenses/osl-3.0.php
+ * If you did not receive a copy of the license and are unable to
+ * obtain it through the world-wide-web, please send an email
+ * to license@magentocommerce.com so we can send you a copy immediately.
+ *
+ * @category  Hackathon
+ * @package   Hackathon_HoneySpam
+ * @author    Andreas Emer <honeyspam@emdec.de>
+ * @author    Fabian Blechschmidt <hackathon@fabian-blechschmidt.de>
+ * @author    Sascha Wohlgemuth <sascha.wohlgemuth@gmail.com>
+ * @author    Bastian Ike <bastian.ike@gmail.com>
+ * @author    Peter Ukener <peterukener@gmail.com>
+ * @copyright 2012 Magento Hackathon
+ * @license   http://opensource.org/licenses/osl-3.0.php  Open Software License (OSL 3.0)
+ * @link      http://www.magento-hackathon.de/
+ */
+class Hackathon_HoneySpam_IndexController extends Mage_Core_Controller_Front_Action
+{
+
+    public function indexAction()
+    {
+        $this->_redirect('honeyspam/error/');
+    }
+
+
+
+}
