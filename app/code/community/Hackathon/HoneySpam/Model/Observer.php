50a1f1173db2ec5fd673b584b62149bba437406d
Fabian Blechschmidt <Fabian.Blech@gmx.de>
2012-10-27 19:36:35 +0200

    added license header cleaned up parameters

--- app/code/community/Hackathon/HoneySpam/Model/Observer.php
+++ app/code/community/Hackathon/HoneySpam/Model/Observer.php
@@ -1,35 +1,56 @@
 <?php
+/**
+ * Magento
+ *
+ * NOTICE OF LICENSE
+ *
+ * This source file is subject to the Open Software License (OSL 3.0)
+ * that is bundled with this package in the file LICENSE.txt.
+ * It is also available through the world-wide-web at this URL:
+ * http://opensource.org/licenses/osl-3.0.php
+ * If you did not receive a copy of the license and are unable to
+ * obtain it through the world-wide-web, please send an email
+ * to license@magentocommerce.com so we can send you a copy immediately.
+ *
+ * @category  Hackathon
+ * @package   Hackathon_HoneySpam
+ * @author    Andreas Emer <honeyspam@emdec.de>
+ * @author    Fabian Blechschmidt <hackathon@fabian-blechschmidt.de>
+ * @author    Sascha Wohlgemuth <sascha.wohlgemuth@gmail.com>
+ * @author    Bastian Ike <bastian.ike@gmail.com>
+ * @author    Peter Ukener <peterukener@gmail.com>
+ * @copyright 2012 Magento Hackathon
+ * @license   http://opensource.org/licenses/osl-3.0.php  Open Software License (OSL 3.0)
+ * @link      http://www.magento-hackathon.de/
+ */
 class Hackathon_HoneySpam_Model_Observer
 {
     /**
      * call rules
-     *
-     * @param $observer Varien_Event_Observer
      */
-    public function controllerActionPredispatchCustomerAccountCreatepost($observer)
+    public function controllerActionPredispatchCustomerAccountCreatepost()
     {
         if (Mage::getStoreConfig('hackathon/honeyspam/enableHoneypotName')) {
-            $this->_checkHoneypot($observer);
+            $this->_checkHoneypot();
         }
 
         if (Mage::getStoreConfig('hackathon/honeyspam/enableHoneypotAccountCreateTime')) {
-            $this->_checkTimestamp($observer);
+            $this->_checkTimestamp();
         }
     }
-    
-    public function controllerActionPredispatchBlockReviewForm() {
+
+    public function controllerActionPredispatchBlockReviewForm()
+    {
         if (Mage::getStoreConfig('hackathon/honeyspam/enableHoneypotName')) {
-                $this->_checkHoneypot($observer);
-            }
-        
+            $this->_checkHoneypot();
+        }
+
     }
-    
+
     /**
      * validate honeypot field
-     *
-     * @param $observer Varien_Event_Observer
      */
-    protected function _checkHoneypot($observer)
+    protected function _checkHoneypot()
     {
         /* @var $helper Hackathon_HoneySpam_Helper_Data */
         $helper = Mage::helper('hackathon_honeyspam');
@@ -40,24 +61,22 @@ class Hackathon_HoneySpam_Model_Observer
 
     /**
      * validate time
-     *
-     * @param $observer Varien_Event_Observer
      */
-    protected function _checkTimestamp($observer)
+    protected function _checkTimestamp()
     {
         $session = Mage::getSingleton('customer/session');
         $accountCreateTime = Mage::getStoreConfig('hackathon/honeyspam/honeypotAccountCreateTime');
-        if (!$session->getAccountCreateTime(false) || ($session->getAccountCreateTime() > (time() - $accountCreateTime))) {
+        if (
+            !$session->getAccountCreateTime(false) || ($session->getAccountCreateTime() > (time() - $accountCreateTime))
+        ) {
             Mage::throwException('Honeypot catched');
         }
     }
 
     /**
      * set access timestamp
-     *
-     * @param $observer Varien_Event_Observer
      */
-    public function controllerActionPredispatchCustomerAccountCreate($observer)
+    public function controllerActionPredispatchCustomerAccountCreate()
     {
         $session = Mage::getSingleton('customer/session');
         $session->setAccountCreateTime(time());
