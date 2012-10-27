9e3ac42fb464a052bc1ac2b1ecd26be5448e346f
thebod <thebod@thebod.de>
2012-10-27 13:59:52 +0200

    added create time check (5sec)

--- app/code/community/Hackathon/HoneySpam/Model/Observer.php
+++ app/code/community/Hackathon/HoneySpam/Model/Observer.php
@@ -1,12 +1,52 @@
 <?php
 class Hackathon_HoneySpam_Model_Observer
 {
-    public function checkHoneypot()
+    /**
+     * call rules
+     *
+     * @param $observer Varien_Event_Observer
+     */
+    public function controllerActionPredispatchCustomerAccountCreatepost($observer)
+    {
+        $this->_checkHoneypot($observer);
+        $this->_checkTimestamp($observer);
+    }
+
+    /**
+     * validate honeypot field
+     *
+     * @param $observer Varien_Event_Observer
+     */
+    protected function _checkHoneypot($observer)
     {
         /* @var $helper Hackathon_HoneySpam_Helper_Data */
         $helper = Mage::helper('hackathon_honeyspam');
         if (strlen(Mage::app()->getRequest()->getParam($helper->getHoneypotName()))) {
             Mage::throwException('Honeypot filled. Aborted.');
         }
+    }
+
+    /**
+     * validate time
+     *
+     * @param $observer Varien_Event_Observer
+     */
+    protected function _checkTimestamp($observer)
+    {
+        $session = Mage::getSingleton('customer/session');
+        if ($session->getAccountCreateTime() > (time() - 5)) {
+            Mage::throwException('Honeypot catched');
+        }
+    }
+
+    /**
+     * set access timestamp
+     *
+     * @param $observer Varien_Event_Observer
+     */
+    public function controllerActionPredispatchCustomerAccountCreate($observer)
+    {
+        $session = Mage::getSingleton('customer/session');
+        $session->setAccountCreateTime(time());
     }
 }
